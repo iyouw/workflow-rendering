34f47d25759b77662641d7d0007b440bb25adf2d
wangxiaobo <wang_xiaobo@hzlh.com>
2022-11-01 16:46:21 +0800

    feat(base): refactor template

--- src/App.vue
+++ src/App.vue
@@ -6,25 +6,25 @@
 </template>
 
 <script setup lang="ts">
-import { onMounted } from 'vue';
-import { FabricPainter } from './core/painter/fabric-painter';
+// import { onMounted } from 'vue';
+// import { FabricPainter } from './core/painter/fabric-painter';
 
-onMounted(()=>{
-  const painter = new FabricPainter('designer');
+// onMounted(()=>{
+//   const painter = new FabricPainter('designer');
 
-  painter.button('开始',undefined, undefined, 60, 60, 30);
+//   painter.button('开始',undefined, undefined, 60, 60, 30);
 
-  painter.button('识别是否有人',undefined, 120, 200, 48, 24);
+//   painter.button('识别是否有人',undefined, 120, 200, 48, 24);
 
-  painter.button('请将左侧节点或算法拖入框中',undefined, 120 + 48 + 30, 200, 48, 0, 'dashed');
+//   painter.button('请将左侧节点或算法拖入框中',undefined, 120 + 48 + 30, 200, 48, 0, 'dashed');
 
-  painter.select('条件判断1', undefined, 120 + 48 + 30 + 48 + 30, 200, 48);
-  painter.warn(undefined, 120 + 48 + 30 + 48 + 30+48);
+//   painter.select('条件判断1', undefined, 120 + 48 + 30 + 48 + 30, 200, 48);
+//   painter.warn(undefined, 120 + 48 + 30 + 48 + 30+48);
 
-  painter.select('条件判断2', undefined, 1070, 200, 48);
+//   painter.select('条件判断2', undefined, 1070, 200, 48);
 
-  painter.render();
-})
+//   painter.render();
+// })
 </script>
 
 <style lang="css" scoped>
